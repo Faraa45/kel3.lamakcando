83c1ac88034813142249b3dd566ecf0c0838c87f
unknown <fararyska425@gmail.com>
2025-03-25 13:17:27 +0700

    masdat menu

---
+++ app/Models/Menu.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Menu extends Model
+{
+    use HasFactory;
+
+    protected $table = 'menu';
+    protected $guarded = [];
+
+    public static function getNoMenu()
+    {
+        // query kode perusahaan
+        $sql = "SELECT IFNULL(MAX(no_menu), 'MN-000') as no_menu
+                FROM menu ";
+        $nomenu = DB::select($sql);
+
+        // cacah hasilnya
+        foreach ($nomenu as $nmn) {
+            $kd = $nmn->no_menu;
+        }
+        // Mengambil substring tiga digit akhir dari string PR-000
+        $noawal = substr($kd,-3);
+        $noakhir = $noawal+1; //menambahkan 1, hasilnya adalah integer cth 1
+        $noakhir = 'MN-'.str_pad($noakhir,3,"0",STR_PAD_LEFT); //menyambung dengan string PR-001
+        return $noakhir;
+
+    }
+
+     // Dengan mutator ini, setiap kali data harga_barang dikirim ke database, koma akan otomatis dihapus.
+     public function setHargaMenuAttribute($value)
+     {
+         // Hapus koma (,) dari nilai sebelum menyimpannya ke database
+         $this->attributes['harga_menu'] = str_replace('.', '', $value);
+     }
+
+}
