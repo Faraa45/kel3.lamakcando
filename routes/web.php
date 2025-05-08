3c5a4069c873c2005d38719d2aca43607d34210b
rizkia13 <sitirizkyatullohkhasanah@gmail.com>
2025-05-08 21:56:01 +0700

    menyelesaikan absensi

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// login costumer
+Route::get('/depan', [App\Http\Controllers\KeranjangController::class,'costumer'])
+     ->middleware(\App\Http\Middleware\CostumerMiddleware::class)
+     ->name('depan');
+
+Route::get('/login', function () {
+    return view('login');
+});
+// tambahan route untuk proses login
+use Illuminate\Http\Request;
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+
+Route::get('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/login');
+})->name('logout');
+
+// untuk ubah password
+Route::get('/ubahpassword', [App\Http\Controllers\AuthController::class, 'ubahpassword'])
+    ->middleware('costumer')
+    ->name('ubahpassword');
+Route::post('/prosesubahpassword', [App\Http\Controllers\AuthController::class, 'prosesubahpassword'])
+    ->middleware('costumer')
+;
+// prosesubahpassword
+
+//absensi
+Route::resource('absensi', AbsensiController::class);
